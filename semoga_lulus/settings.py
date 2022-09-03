4e93a978dbf1494c0297aa858fe843ef8d24c944
Mayfa Shadrina Siddi <mayfa.shadrina@ui.ac.id>
2022-09-03 21:48:24 +0700

    third commit: settings rev

--- semoga_lulus/settings.py
+++ semoga_lulus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qrj96x#vfoxirtn_jup898-nq0rgr!(145)ln*(t7i6_bmu=c-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'semoga_lulus.urls'
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
